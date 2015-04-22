4be12ddbc77c28f89adec090501d2650266e7964
Brian Hines <brian@projectweekend.net>
2015-04-22 05:37:22 -0500

    Use yml config file

--- collector.py
+++ collector.py
@@ -1,28 +1,34 @@
-import os
+import yaml
 import newspaper
 from newspaper import news_pool
 
 
-SOURCE_URL = os.getenv('SOURCE_URL', None)
-assert SOURCE_URL
-TARGET_WORDS = os.getenv('TARGET_WORDS', None)
-assert TARGET_WORDS
-THREADS = os.getenv('THREADS', '2')
+def get_config():
+    with open('/options/config.yml') as file:
+        config = yaml.safe_load(file)
+    keys = config.keys()
+    if not 'threads' in keys:
+        config['threads'] = 2
+    if not 'source' in keys:
+        raise Exception('"source" not defined in config.yml')
+    if not 'words' in keys:
+        raise Exception('"words" not defined in config.yml')
+    return config
 
 
 def main():
-    target_words_set = set([i.strip() for i in TARGET_WORDS.split(',')])
-    num_of_threads = int(THREADS)
-
-    paper = newspaper.build(SOURCE_URL)
-    news_pool.set([paper], threads_per_source=num_of_threads)
+    config = get_config()
+    target_words_set = set([i.lower() for i in config['words']])
+    paper = newspaper.build(config['source'])
+    news_pool.set([paper], threads_per_source=config['threads'])
     news_pool.join()
 
     for a in paper.articles:
         a.parse()
         a.nlp()
-        if set(a.keywords) < target_words_set:
+        if target_words_set < set(a.keywords):
             print(a.title)
+            print(a.keywords)
             # TODO: hand article to queue for processing
 
 
